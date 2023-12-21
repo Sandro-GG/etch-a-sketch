05cd22fe142a449db5a73882f4316c5674622df7
Sandro Gakharia <sandrogakharia@gmail.com>
2023-12-21 21:49:33 +0400

    Improve layout and fix bugs

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const square = [];
 const container = document.querySelector('.container');
+const grid = document.createElement('div');
+grid.classList.add('grid');
 let n = 16;
 
-const btn = document.querySelector('#btn');
-btn.addEventListener('click', () => {
-  n = prompt("How many squares per side?")
+const changeSize = document.querySelector('#btn');
+
+changeSize.addEventListener('click', () => {
+  removeGrid(n);
+  n = prompt("How many squares per side?");
   makeGrid(n);
-})
+});
+
+makeGrid(n);
 
 function makeGrid(n) {
   for (let i = 0; i < n * n; i++) {
@@ -14,10 +20,17 @@ function makeGrid(n) {
     square[i].classList.add('square');
     square[i].addEventListener('mouseover', () => {
       square[i].style.backgroundColor = 'black';
-    })
-    container.appendChild(square[i]);
+    });
+    grid.appendChild(square[i]);
     square[i].style.width = `calc(${100 / n}%)`;
     square[i].style.height = 0;
     square[i].style.paddingBottom = `calc(${100 / n}%)`; 
   };
+  container.appendChild(grid);
+}
+
+function removeGrid() {
+  for (let i = 0; i < n * n; i++) {
+    grid.removeChild(square[i]);
+  }
 }
